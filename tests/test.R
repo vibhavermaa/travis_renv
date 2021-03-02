44263fef7c7f08348461f69236591fced3f76f08
Vibha Verma <vibha_verma@external.mckinsey.com>
2021-03-02 16:37:48 +0530

    renv

---
+++ tests/test.R
@@ -0,0 +1,27 @@
+app <- ShinyDriver$new("..")
+app$snapshotInit("test")
+app$snapshot()
+app$setInputs(bins = 50)
+app$snapshot()
+app$setInputs(bins = 1)
+app$snapshot()
+
+
+# serverAddr <- "localhost"
+# driver_port <- 4444
+# browserName <-  "chrome"
+
+# eCaps <- list(chromeOptions = list(
+#   args = c('--headless', '--disable-gpu', '--window-size=1280,800')))
+
+# remDr <- remoteDriver(extraCapabilities = eCaps)
+# # remDr <- remoteDriver(remoteServerAddr=serverAddr,port=driver_port,browserName=browserName)
+# remDr$open()
+# 
+# 
+# app <- remoteDriver$new("..")
+# #we need port serveraddr and borser name along with URL
+# # val <- document.getElementsByClassName("container-fluid")[0].innerText["length"]
+# expect_equal("67", "67")
+# 
+# remDr$close()
